4da58c6c995f365d92fa2834d51135d014a32187
ha3a <harri.heikkila92@gmail.com>
2017-01-26 23:49:25 +0200

    Koodia ja testejä

---
+++ Pasianssi/Pasianssi/src/main/java/pasianssi/pasianssi/Kortti.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasianssi.pasianssi;
+
+/**
+ *Kortti luokka yksittäiselle kortille
+ * @author Harri
+ */
+public class Kortti {
+
+    //Mikä maaID on mikäkin maa
+    //Punaiset ovat parittomia
+    public static final int HERTTA = 1;
+    public static final int PATA = 2;
+    public static final int RUUTU = 3;
+    public static final int RISTI = 4;
+
+    private final String[] maittenNimet = new String[]{
+        "Hearts", "Spades", "Diamond", "Clubs"
+    };
+
+    //Mikä kortti on minkäkin niminen
+    public static final int ASSA = 1;
+    public static final int JATKA = 11;
+    public static final int KUNIGATAR = 12;
+    public static final int KUNINGAS = 13;
+
+    private final String[] korttienNimet = new String[]{
+        "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
+    };
+
+    // 1-13 hertta
+    // 14-26 pata
+    // 27-39 ruutu
+    // 40-52 risti
+    private final int pakkaArvo; // 1-52
+    private final int kortinArvo; // 1-13
+    private final int maanArvo; // 1-4
+
+    
+    //Käytetty konstruktori
+    public Kortti(int pakkaArvo) {
+        if ((pakkaArvo < 1 || pakkaArvo > 52)) {
+            throw new IllegalArgumentException("Virheellinen arvo");
+        }
+        this.pakkaArvo = pakkaArvo;
+        this.maanArvo = pakkaArvoMaanArvoksi(pakkaArvo);
+        this.kortinArvo = pakkaArvoKortinArvoksi(pakkaArvo);
+
+    }
+
+    
+   
+
+    //gettereitä
+    public int getPakkaArvo() {
+        return pakkaArvo;
+    }
+
+    public int getMaanArvo() {
+        return maanArvo;
+    }
+
+    public String getKortinMaanNimi() {
+        return maittenNimet[maanArvo - 1];
+    }
+
+    public int getKortinArvo() {
+        return kortinArvo;
+    }
+
+    public String getKortinArvonNimi() {
+        return korttienNimet[kortinArvo - 1];
+    }
+
+    //PakkaArvon muuntaminen kortin arvoksi
+    public static int pakkaArvoKortinArvoksi(int pakkaArvo) {
+        int kortinArvo = (pakkaArvo % 13);
+        if(kortinArvo == 0){
+            kortinArvo = 13;
+        }
+
+        return kortinArvo;
+    }
+
+    //Kortin tulostus lyhyessä muodossa esimerkiksi ruutu 10 on 10C
+    public String toString() {
+        String korttiArvoTekstina;
+        if (kortinArvo > 1 && kortinArvo < 11) {
+            korttiArvoTekstina = getKortinArvonNimi();
+        } else {
+            korttiArvoTekstina = getKortinArvonNimi().substring(0, 1);
+        }
+        String maaLyhyena = getKortinMaanNimi().substring(0, 1);
+        String lyhytNimi = korttiArvoTekstina + maaLyhyena;
+
+        return lyhytNimi;
+
+    }
+
+    //Kortin koko nimen tulostus
+    public String toStringPitka() {
+        String kortinNimi = getKortinMaanNimi() + " " + getKortinArvonNimi();
+        return kortinNimi;
+    }
+
+    //KortinArvon ja maanArvon muuntaminen pakkaArvoksi
+    public static int pakkaArvoksi(int kortinArvo, int maanArvo) {
+        if (kortinArvo < 1 || kortinArvo > 13) {
+            throw new IllegalArgumentException("Virheellinen kortin arvo");
+        }
+        if (maanArvo < 1 || maanArvo > 4) {
+            throw new IllegalArgumentException("Virheellinen maan arvo");
+        }
+        int pakkaArvo = (13 * (maanArvo - 1)) + kortinArvo;
+        return pakkaArvo;
+    }
+
+    //PakkaArvosta muunto maa arvoksi
+    public static int pakkaArvoMaanArvoksi(int pakkaArvo) {
+        int maanArvo = pakkaArvo / 13;
+        if (pakkaArvo % 13 != 0) {
+            maanArvo++;
+        }
+        return maanArvo;
+    }
+
+}
---
+++ Pasianssi/Pasianssi/src/test/java/pasianssi/pasianssi/KorttiTest.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasianssi.pasianssi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Harri
+ */
+public class KorttiTest {
+
+    public KorttiTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testLuoKortit() {
+        for (int i = 1; i <= 52; i++) {
+            Kortti kortti = new Kortti(i);
+        }
+    }
+
+    //Antaako getPakkaArvo oikean arvon
+    @Test
+    public void testGetPakkaArvo() {
+        Kortti kortti = new Kortti(1);
+        int i = kortti.getPakkaArvo();
+        assertEquals(1, i);
+    }
+
+    
+    //Sama max valuella
+    @Test
+    public void testGetPakkaArvoMax() {
+        Kortti kortti = new Kortti(52);
+        int i = kortti.getPakkaArvo();
+        assertEquals(52, i);
+    }
+
+    //Antaako getMaanArvo oikean arvon
+    @Test
+    public void testGetMaanArvo() {
+        Kortti kortti = new Kortti(1);
+        int i = kortti.getMaanArvo();
+        assertEquals(1, i);
+    }
+
+    
+    //Sama max valuella
+    @Test
+    public void testGetMaanArvoMax() {
+        Kortti kortti = new Kortti(52);
+        int i = kortti.getMaanArvo();
+        assertEquals(4, i);
+    }
+
+    //Antaako getKortinMaanNimi oikean nimen
+    @Test
+    public void testGetKortinMaanNimi() {
+        Kortti kortti = new Kortti(1);
+        String nimi = kortti.getKortinMaanNimi();
+        assertEquals("Hearts", nimi);
+    }
+
+    
+    //Sama max valuella
+    @Test
+    public void testGetKortinMaanNimiMax() {
+        Kortti kortti = new Kortti(52);
+        String nimi = kortti.getKortinMaanNimi();
+        assertEquals("Clubs", nimi);
+    }
+
+    // Antaako getKortinArvo oikean arvon
+    @Test
+    public void testGetKortinArvo() {
+        Kortti kortti = new Kortti(1);
+        int i = kortti.getKortinArvo();
+        assertEquals(1, i);
+    }
+
+    //Sama max valuella
+    @Test
+    public void testGetKortinArvoMax() {
+        Kortti kortti = new Kortti(52);
+        int i = kortti.getKortinArvo();
+        assertEquals(13, i);
+    }
+
+    //Antaako getKortinArvonNimi oikean nimen
+    @Test
+    public void testGetKortinArvonNimi() {
+        Kortti kortti = new Kortti(1);
+        String nimi = kortti.getKortinArvonNimi();
+        assertEquals("Ace", nimi);
+    }
+
+    
+    //Tulostaako kortti oikean nimen
+    @Test
+    public void testGetKortinArvonNimiMax() {
+        Kortti kortti = new Kortti(52);
+        String nimi = kortti.getKortinArvonNimi();
+        assertEquals("King", nimi);
+    }
+
+    
+    //Muuntuuko pakkaArvo kortinArvoksi oikein
+    @Test
+    public void testpakkaArvoKortinArvoksi() {
+        Kortti kortti = new Kortti(52);
+        int i = Kortti.pakkaArvoKortinArvoksi(kortti.getPakkaArvo());
+        assertEquals(13, i);
+    }
+
+    //Toimiiko lyhyt tulostus oikein
+    @Test
+    public void testToString() {
+        Kortti kortti = new Kortti(1);
+        String nimi = kortti.toString();
+        assertEquals("AH", nimi);
+    }
+
+    //Toimiiko pitkä tulostus oikein
+    @Test
+    public void testToStringPitka() {
+        Kortti kortti = new Kortti(1);
+        String nimi = kortti.toStringPitka();
+        assertEquals("Hearts Ace", nimi);
+    }
+
+    //Muuntuuko kortinArvo ja maanArvo pakkaArvoksi oikein
+    @Test
+    public void testPakkaArvoksi() {
+        Kortti kortti = new Kortti(52);
+        int i = Kortti.pakkaArvoksi(kortti.getKortinArvo(), kortti.getMaanArvo());
+        assertEquals(52, i);
+    }
+
+    //Muuntuuko pakkaArvo maanArvoksi oikein
+    @Test
+    public void testPakkaArvoMaanArvoksi() {
+        Kortti kortti = new Kortti(52);
+        int i = Kortti.pakkaArvoMaanArvoksi(kortti.getPakkaArvo());
+        assertEquals(4, i);
+    }
+
+}
